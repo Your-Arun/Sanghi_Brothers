43915f4eb7b54c3d44d045ad2d3af76529d9f122
Your-Arun <arun.work82@gmail.com>
2025-02-24 13:16:40 +0530

    uplod

--- Frontend/src/components/Home Page/Signup.jsx
+++ Frontend/src/components/Home Page/Signup.jsx
@@ -107,9 +107,9 @@ const Signup = () => {
           onChange={(e) => setDepartment(e.target.value)}
           required
         >
-          <option value="sales">Sales</option>
-          <option value="finance">Finance</option>
-          <option value="accounts">Accounts</option>
+          <option value="manager">Manager</option>
+          <option value="accounts">Accounts/Finance</option>
+          <option value="backoffice">Back Office</option>
         </select>
         
         <button className="w-full bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-300">
