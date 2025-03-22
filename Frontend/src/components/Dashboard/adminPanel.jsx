158040fbd9cafcba28e5df20b3df4a6e7ab818a7
Your-Arun <arun.work82@gmail.com>
2025-03-22 18:24:27 +0530

    2

--- Frontend/src/components/Dashboard/adminPanel.jsx
+++ Frontend/src/components/Dashboard/adminPanel.jsx
@@ -60,6 +60,7 @@ const AdminPanel = () => {
   };
 
   return (
+    <>
     <div className="p-6 bg-gray-100 min-h-screen">
       
       <h1 className="text-3xl font-bold text-blue-600 text-center mb-6">Admin Panel - Manage Users</h1>
@@ -146,6 +147,8 @@ const AdminPanel = () => {
         </div>
       )}
     </div>
+    <div> // profile me dalva hai hint </div>
+    </>
   );
 };
 
