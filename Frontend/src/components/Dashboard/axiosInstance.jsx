26a09ff2c903f2d68d07c05ac051697e34b13488
Your-Arun <arun.work82@gmail.com>
2025-04-02 13:13:59 +0530

    2

--- Frontend/src/components/Dashboard/axiosInstance.jsx
+++ Frontend/src/components/Dashboard/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5500",
+  baseURL: "https://test-backend-pv0s.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
