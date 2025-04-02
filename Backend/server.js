941e50d27d52301e0a75175a45c364ff3d1d5652
Your-Arun <arun.work82@gmail.com>
2025-04-02 14:04:03 +0530

    22

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 // 🛡️ CORS Configuration
 app.use(
   cors({
-    origin: "http://localhost:5173", // Frontend URL
+    origin: "https://test-frontend-nyqt.onrender.com", // Frontend URL
     credentials: true, // ✅ Allow cookies & authentication headers
   })
 );
