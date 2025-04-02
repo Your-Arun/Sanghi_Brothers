090f52c2f994f6d57eddc0e4432de628376c008b
Your-Arun <arun.work82@gmail.com>
2025-04-02 14:09:01 +0530

    33

--- Frontend/src/components/Dashboard/ProtectedRoute.jsx
+++ Frontend/src/components/Dashboard/ProtectedRoute.jsx
@@ -1,35 +1,14 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { useLocation } from "react-router-dom";
-import { useState, useEffect } from "react";
 
 const ProtectedRoute = () => {
   const authToken = sessionStorage.getItem("authToken");
-  const location = useLocation();
-  const [showAlert, setShowAlert] = useState(false);
-
-  useEffect(() => {
-    if (!authToken && location.pathname !== "/login" && location.pathname !== "/signup") {
-      setShowAlert(true);
-      setTimeout(() => {
-        sessionStorage.removeItem("authToken");
-        window.location.href = "/login";
-      }, 2000);
-    }
-  }, [authToken, location]);
-
-  if (showAlert) {
-    return (
-      <div>
-        <p>You are not authorized to access this page.</p>
-        <p>You will be redirected to the login page in 2 seconds.</p>
-      </div>
-    );
-  }
 
   return authToken ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
 export default ProtectedRoute;
+
+
 
 
 
