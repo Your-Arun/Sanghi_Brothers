67705c8d515f8e6c03219ae51f315b90b0637f3c
Your-Arun <arun.work82@gmail.com>
2025-06-16 13:46:38 +0530

    s

--- Frontend/src/components/Layouts/Sb01.jsx
+++ Frontend/src/components/Layouts/Sb01.jsx
@@ -206,7 +206,7 @@ const Sb01 = () => {
   return (
     <>
       <div className="bg-gradient-to-r from-blue-400 to-yellow-400 items-center justify-center min-h-screen p-6">
-        <form onSubmit={handleSave}>
+        <form className=" min-h-screen " onSubmit={handleSave}>
           <div>
             <div className="text-center text-3xl p-4">
               <h1>
