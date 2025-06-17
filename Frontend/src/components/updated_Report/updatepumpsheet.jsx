7bbb5fda8eed29cee6b893e53bd7def4ca053e33
Your-Arun <arun.work82@gmail.com>
2025-06-17 12:54:34 +0530

    w

--- Frontend/src/components/updated_Report/updatepumpsheet.jsx
+++ Frontend/src/components/updated_Report/updatepumpsheet.jsx
@@ -4,6 +4,9 @@ import previousImage from "/previous.png";
 import binImage from "/bin.png";
 import saveImage from "/save.png";
 import { Link, useNavigate, useParams } from "react-router-dom";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const UpdatePumpSheet = () => {
   const { id } = useParams();
@@ -106,23 +109,21 @@ const UpdatePumpSheet = () => {
       )
     }
   
-    const handleDelete = async (e) => {
-      e.preventDefault();
-      confirmDeleteToast(async () => {
-        try {
-          await axiosInstance.delete(`/mastersheet/pumpsheet/${id}`);
-          alert("Report deleted successfully!");
-          navigate("/mastersheet");
-        } catch (error) {
-          alert("Failed to delete report.");
-          console.error("Delete error:", error);
-        }
-      });
-    };
-    
+  const handleDelete = async (e) => {
+    e.preventDefault()
+    confirmDeleteToast(async () => {
+      try {
+        await axiosInstance.delete(`/mastersheet/pumpsheet/${id}`);
+        alert("Report deleted successfully!");
+        navigate("/mastersheet"); // Redirect to dashboard or another page
+      } catch (error) {
+        alert("Failed to delete report.");
+      }
+    })
+  }
 
   return (
-    <div className="flex flex-col items-center  bg-gradient-to-r from-blue-400 to-yellow-400 justify-center min-h-screen bg-gray-100 p-6">
+    <div>
       <h1 className="text-center text-xl p-4">PUMP REPORT SHEET</h1>
       <form>
         <div className="text-center mt-[-20px] text-xl p-4">
