e7574957c9f49eb539c908497491120d205f0de7
Your-Arun <arun.work82@gmail.com>
2025-06-21 17:14:46 +0530

    w

--- Frontend/src/components/Layouts/ReportFile.jsx
+++ Frontend/src/components/Layouts/ReportFile.jsx
@@ -200,7 +200,7 @@ function ReportFile() {
   return (
     <>
       {" "}
-      <div className="bg-gradient-to-r from-blue-400 to-yellow-400 p-4">
+      <div className="bg-gradient-to-r from-blue-400 to-yellow-400 p-6 items-center justify-center ">
         <form onSubmit={handleSubmit}>
           <div>
             <h1 className="text-3xl mt-5  font-thin text-center  text-red-950  mb-4">
