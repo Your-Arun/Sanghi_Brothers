2ec6a5089156135938138a40636b8fbb0321d4f1
Your-Arun <arun.work82@gmail.com>
2025-06-24 16:33:54 +0530

    e

--- Frontend/src/components/Layouts/Report.jsx
+++ Frontend/src/components/Layouts/Report.jsx
@@ -45,7 +45,7 @@ const Report = () => {
 
   return (
     <>
-      <div className="flex flex-col bg-gradient-to-r from-blue-400 to-yellow-400 items-center justify-center">
+      <div className="flex flex-col bg-gradient-to-r from-blue-400 to-yellow-400 p-6 items-center justify-center min-h-screen">
         <form className="bg-white p-6 rounded shadow-md " onSubmit={handleSubmit}>
           <h2 className="text-4xl text-center font-bold mb-4">
             COMPLAINTs
