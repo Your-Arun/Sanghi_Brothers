a1bec5a6714d28175c2d65c06619dfa41b6744bf
Your-Arun <arun.work82@gmail.com>
2025-06-26 12:08:56 +0530

    saa

--- Frontend/src/components/Layouts/lekhajokha.jsx
+++ Frontend/src/components/Layouts/lekhajokha.jsx
@@ -109,7 +109,7 @@ const Lekhajokha = () => {
     };
 
     return (
-        <div className="relative p-6 bg-gradient-to-r from-gray-200 to-white min-h-screen">
+        <div className="relative p-6 bg-gradient-to-r from-blue-400 to-yellow-400 min-h-screen">
             <form onSubmit={handleSubmit}>
                 <h1 className="text-5xl font-bold text-gray-800 text-center">Lekha Jokha</h1>
                 <div className="flex justify-end mb-6">
