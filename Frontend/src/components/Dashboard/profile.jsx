6b811352c66dbdeaf90540695cd2b6453a4f6e78
Arun Kumar <156221727+Your-Arun@users.noreply.github.com>
2025-07-07 17:17:05 +0530

    fdf

--- Frontend/src/components/Dashboard/profile.jsx
+++ Frontend/src/components/Dashboard/profile.jsx
@@ -59,9 +59,8 @@ const ProfileModal = ({ closeModal }) => {
           {/* Name */}
           <div>
             <label className="block text-sm font-semibold text-gray-700 mb-1">Name:</label>
-            <p 
-              className="w-full px-3 py-2 border border-gray-300 rounded-md bg-gray-100 text-sm"
-            >{username}<p/>
+            <p className="w-full px-3 py-2 border border-gray-300 rounded-md bg-gray-100 text-sm"
+            >{username}</p>
           </div>
 
           {/* Email */}
