8663f7136e805868fe22138f8a9b2434fc100729
Arun Kumar <156221727+Your-Arun@users.noreply.github.com>
2025-07-07 17:13:07 +0530

    dfdf

--- Frontend/src/components/Dashboard/profile.jsx
+++ Frontend/src/components/Dashboard/profile.jsx
@@ -63,7 +63,7 @@ const ProfileModal = ({ closeModal }) => {
               value={username}
               onChange={(e) => setUsername(e.target.value)}
               className="w-full px-3 py-2 border border-gray-300 rounded-md bg-gray-100 text-sm"
-            />
+            />{username}
           </div>
 
           {/* Email */}
