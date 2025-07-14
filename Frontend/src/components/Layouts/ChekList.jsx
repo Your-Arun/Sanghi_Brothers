f16a79adcad1f8b8212e1d920fb17c56bca1e1a8
Arun Kumar <156221727+Your-Arun@users.noreply.github.com>
2025-07-14 12:52:54 +0530

    ffg

--- Frontend/src/components/Layouts/ChekList.jsx
+++ Frontend/src/components/Layouts/ChekList.jsx
@@ -58,7 +58,7 @@ const ChekList = () => {
 
   return (
     <div className="flex flex-col items-center justify-center p-6">
-      <h1 className="text-6xl font-bold text-center text-gray-80 mt-5">MASTER SHEET</h1>
+      <h1 className="text-6xl font-bold text-center text-gray-80 mb-4">MASTER SHEET</h1>
       
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {sections.map(({ key, title, route }) => (
