3c6f1aa504fbbd679df5046b09f4541d0f60616a
Arun Kumar <156221727+Your-Arun@users.noreply.github.com>
2025-07-22 15:41:09 +0530

    dfdf

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GoogleOAuthProvider clientId="1018200022031-f2769na5s91k2f94f4plkbdhnfpsvgnt.apps.googleusercontent.com">
     <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
