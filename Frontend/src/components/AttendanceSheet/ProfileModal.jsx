05996eb030098fd4bca3cce8e51d21a29c743835
Arun Kumar <156221727+Your-Arun@users.noreply.github.com>
2025-07-29 18:19:43 +0530

    ffg

--- Frontend/src/components/AttendanceSheet/ProfileModal.jsx
+++ Frontend/src/components/AttendanceSheet/ProfileModal.jsx
@@ -35,12 +35,12 @@ const ProfileModal = ({ user, onClose, onUpdate }) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center z-50">
       <div className="bg-gray-900 text-white w-full max-w-3xl p-6 rounded-lg shadow-xl relative max-h-[90vh] overflow-y-auto">
-        <button
+        <div
           onClick={onClose}
           className="absolute top-2 right-4 text-2xl text-red-400 hover:text-red-600"
         >
           &times;
-        </button>
+        </div>
 
         <div className="flex items-center gap-6 mb-6">
           <img
