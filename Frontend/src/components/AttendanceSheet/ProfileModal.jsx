55c67c0ab3c1bb6f9535ff557beb825dda7691c4
Arun Kumar <156221727+Your-Arun@users.noreply.github.com>
2025-07-29 18:43:40 +0530

    vbvb

--- Frontend/src/components/AttendanceSheet/ProfileModal.jsx
+++ Frontend/src/components/AttendanceSheet/ProfileModal.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { UserContext } from "../Home Page/UserContext";
+import  UserContext  from "../Home Page/UserContext";
 import axiosInstance from "../Dashboard/axiosInstance";
 import { toast } from "react-toastify";
 
