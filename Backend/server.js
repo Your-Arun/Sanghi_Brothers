8bdc62e3dd4229ffc4caa13a1faf7379488437cc
Arun Kumar <156221727+Your-Arun@users.noreply.github.com>
2025-08-05 17:12:17 +0530

    f

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,9 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+
 app.use(cookieParser());
 
 // 🛡️ CORS Configuration
@@ -22,8 +25,7 @@ app.use(
 app.use(express.json());
 
 
-app.use(express.json({ limit: "5mb" }));
-app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+
 
 
 app.use(session({
