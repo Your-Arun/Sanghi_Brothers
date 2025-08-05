227b01b5714aaa0203cfce78b9ff203ba45069a0
Arun Kumar <156221727+Your-Arun@users.noreply.github.com>
2025-08-05 17:47:48 +0530

    rr

--- Backend/server.js
+++ Backend/server.js
@@ -9,8 +9,8 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(express.json({ limit: "5mb" }));
-app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+app.use(express.json({ limit: "2mb" }));
+app.use(express.urlencoded({ extended: true, limit: "2mb" }));
 
 app.use(cookieParser());
 
