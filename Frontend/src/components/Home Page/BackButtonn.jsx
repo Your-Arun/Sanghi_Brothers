6e29b7e9c515e18a2c6d02d10baaa4b58136fa91
Arun Kumar <156221727+Your-Arun@users.noreply.github.com>
2025-08-11 18:26:37 +0530

    fty

--- Frontend/src/components/Home Page/BackButtonn.jsx
+++ Frontend/src/components/Home Page/BackButtonn.jsx
@@ -5,12 +5,14 @@ const BackButton = ({ label = "Back" }) => {
   const navigate = useNavigate();
 
   return (
-    <button
-      onClick={() => navigate(-1)}
-      className="flex justify-center align-center bg-gray-700 hover:bg-gray-600 text-white px-3 py-1 rounded shadow flex items-center gap-2"
-    >
-      ← {label}
-    </button>
+    <div className="flex justify-center mt-6 mb-4">
+      <button
+        onClick={() => navigate(-1)}
+        className="bg-gray-700 hover:bg-gray-600 text-white px-4 py-2 rounded shadow flex items-center gap-2"
+      >
+        ← {label}
+      </button>
+    </div>
   );
 };
 
