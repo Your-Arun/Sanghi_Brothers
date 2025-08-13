d5a58e5d9db9c63cb366d5c8acdc401c850b5863
Arun Kumar <156221727+Your-Arun@users.noreply.github.com>
2025-08-13 16:28:22 +0530

    v

--- Frontend/src/components/Layouts/Sb01.jsx
+++ Frontend/src/components/Layouts/Sb01.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import axiosInstance from '../Dashboard/axiosInstance'
 import saveImage from "/save.png";
 import previousImage from "/previous.png";
