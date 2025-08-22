8456b9384e4d4d07a94ba68a2403a53c65185529
Arun Kumar <156221727+Your-Arun@users.noreply.github.com>
2025-08-22 13:12:47 +0530

    f

--- Frontend/src/components/updated_Report/UpdateReportFile.jsx
+++ Frontend/src/components/updated_Report/UpdateReportFile.jsx
@@ -232,25 +232,11 @@ const UpdateReportFile = () => {
 
   const [isMobile, setIsMobile] = useState(false);
   useEffect(() => {
-      const checkDevice = () => {
-        const isMobileDevice = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
-        const isSmallScreen = window.innerWidth < 1024;
-    
-        if (isMobileDevice && isSmallScreen) {
-          // Mobile + chhoti screen = block
-          setIsMobile(true);
-        } else {
-          // Desktop ya Mobile Desktop Mode
-          setIsMobile(false);
-        }
-      };
-    
-      checkDevice(); // initial check
-      window.addEventListener("resize", checkDevice); // resize par bhi check
-    
-      return () => window.removeEventListener("resize", checkDevice);
-    }, []);
-    
+    // screen width check
+    if (window.innerWidth < 1024) {
+      setIsMobile(true);
+    }
+  }, []);
 
   if (isMobile) {
     return (
