d396c8c9a01fcbb5da8bac3d4aaa589ec6a56dac
Your-Arun <arun.work82@gmail.com>
2025-09-02 00:33:35 +0530

    hgjgj

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -159,3 +159,4 @@ export default App;
 
 //login signup ki dikkt hai signup krne pr success hora hai pr login nhh hora usi id password se 
 //agr signup me passhashed krta hu to attnedance sheet me dikkt dega ..crreate krne time
+//staff panel me foto ka krna hai 
