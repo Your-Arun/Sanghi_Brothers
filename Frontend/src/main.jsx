391752fad670686111c69c20cd008581f00f30ab
Your-Arun <arun.work82@gmail.com>
2025-09-02 13:55:40 +0530

    sd

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,10 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const clientId = "554957789360-s9oa7fe7jso81mhjbqms1diehdupsuhl.apps.googleusercontent.com";
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={clientId}>
+
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
