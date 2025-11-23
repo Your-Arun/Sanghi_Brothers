2605b014c7543b4567f37225f525ec263126e45f
Your-Arun <arun.work82@gmail.com>
2025-11-23 20:31:12 +0530

    hghj

--- Frontend/src/components/Layouts/ShiftingProgram.jsx
+++ Frontend/src/components/Layouts/ShiftingProgram.jsx
@@ -708,7 +708,7 @@ const ShiftManagementSystem = () => {
         </div>
       )}
      {showMemberListModal && (
-  <div className="fixed inset-0 z-[60] flex items-center justify-center bg-black/60 backdrop-blur-sm">
+  <div className="fixed inset-0 z-[60] flex items-center justify-center bg-black/60 backdrop-blur-sm w-[60%] h-[70%]">
     <div className="bg-white w-full max-w-sm rounded-xl p-4 shadow-xl animate-in fade-in duration-150">
 
       {/* Header */}
